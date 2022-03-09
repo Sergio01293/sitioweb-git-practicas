d80e450aa25002fca3e5b29d254540b8a9cf245b
sergio <sergio99_tec@outlook.com>
2022-03-09 01:15:02 -0600

    modificando

---
+++ proyecto con php/cabecera.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Required meta tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Metro 4 -->
+    <link rel="stylesheet" href="https://cdn.metroui.org.ua/v4/css/metro-all.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+
+    <ul class="h-menu">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Portafolio</a></li>
+        <li><a href="#">Cerrar Sesion</a></li>
+    </ul>
--- proyecto con php/index.php
+++ proyecto con php/index.php
@@ -1,3 +1,9 @@
-<?include("cabecera.php");?>
-    <h1>Hola mundo</h1>
+<?php include("cabecera.php");?>
+    <div class="p-5 bg-light">
+        <div class="container">
+            <p class="h3 text-center">Bienvenidos</p>
+            <p>Ven y conoce la mejor experiencia</p>
+            <p>Mas informacion</p>
+        </div>
+    </div>
 <?php include("pie.php")?>
